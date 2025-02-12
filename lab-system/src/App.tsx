6c4e648f5a1e1d3826c02370563c1e46ed28bd2a
MayanV <168961026+MayanV@users.noreply.github.com>
2025-02-12 20:41:06 +0200

    Added dark mode + hotfix
    added dark mode + fixed wrong package installation location

--- lab-system/src/App.tsx
+++ lab-system/src/App.tsx
@@ -1,16 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ComputerManagement from "./pages/ComputerManagement";
-import Dashboard from "./pages/Dashboard";
-import PageNotFound from "./pages/PageNotFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ComputerManagement from './pages/ComputerManagement';
+import Dashboard from './pages/Dashboard';
+import PageNotFound from './pages/PageNotFound';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ComputerManagement />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ComputerManagement />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
