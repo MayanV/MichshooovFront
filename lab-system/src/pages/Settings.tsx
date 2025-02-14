dabf356bc0e811d4f98b4b3b214b93a5098f43d1
MayanV <168961026+MayanV@users.noreply.github.com>
2025-02-14 22:45:42 +0200

    Update Settings.tsx

--- lab-system/src/pages/Settings.tsx
+++ lab-system/src/pages/Settings.tsx
@@ -1,5 +1,16 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function Settings() {
-  return <Typography>This is the settings page</Typography>;
+  return (
+    <>
+      <Box sx={{ margin: '4rem', textAlign: 'center' }}>
+        <Typography variant="h3" gutterBottom>
+          Lol no settings yet
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          womp womp
+        </Typography>
+      </Box>
+    </>
+  );
 }
